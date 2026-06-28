e5afc6b8be18964d65ee1ae847151e4e4328b2fe
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:45 +0600

    thread18

--- src/thread/thread18.ts
+++ src/thread/thread18.ts
@@ -1,9 +1,9 @@
-/* 
-	Дописать функцию getUserWithFallback, которая пытается получить пользователя с помощью fetchUser, а при ошибке пробует получить данные пользователя c помощью fetchUserData.
+﻿/* 
+Дописать функцию getUserWithFallback, которая пытается получить пользователя с помощью fetchUser, а при ошибке пробует получить данные пользователя c помощью fetchUserData.
 */
 
 import { fetchUser, fetchUserData, type User } from "./promises";
 
 export function getUserWithFallback(id: number): Promise<User> {
-  // TODO: Реализовать fallback механизм
+  return fetchUser(id).catch(() => fetchUserData(id));
 }
