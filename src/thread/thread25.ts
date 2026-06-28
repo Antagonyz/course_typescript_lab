214f6a85aefcabd06d234b8da28657f415984696
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:47 +0600

    thread25

--- src/thread/thread25.ts
+++ src/thread/thread25.ts
@@ -1,14 +1,17 @@
-/* 
-	Используйте ResultAsync для асинхронной операции и преобразуйте Promise mockFetch в Promise<Result<number, string>>. В случае ошибки нужно вернуть сообщение "Fetch failed: _код_ошибки_".
+﻿/* 
+Используйте ResultAsync для асинхронной операции и преобразуйте Promise mockFetch в Promise<Result<number, string>>. В случае ошибки нужно вернуть сообщение "Fetch failed: _код_ошибки_".
 */
 
 import { ResultAsync } from 'neverthrow'
 
 export const fetchNumberFromAPI = (url: string): ResultAsync<number, string> => {
-  // Ваш код здесь (3-5 строк)
+return ResultAsync.fromPromise(
+mockFetch(url),
+error => `Fetch failed: ${String(error)}`
+)
 }
 
 // Mock API функция
 const mockFetch = (url: string): Promise<number> => {
-  return url === "success" ? Promise.resolve(42) : Promise.reject("API error")
+return url === "success" ? Promise.resolve(42) : Promise.reject("API error")
 }
