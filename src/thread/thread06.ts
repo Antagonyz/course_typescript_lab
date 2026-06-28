fd5672c5d7ecda974fb3ea2418e2add2f38016ba
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:43 +0600

    thread06

--- src/thread/thread06.ts
+++ src/thread/thread06.ts
@@ -1,16 +1,18 @@
-/* 
-	 Допишите функцию, которая бросает кастомную ошибку с дополнительными данными. В случае, если возраст меньше 18, функция должна бросить ошибку ValidationError с сообщением "Age must be at least 18" и полем "age".
+﻿/* 
+ Допишите функцию, которая бросает кастомную ошибку с дополнительными данными. В случае, если возраст меньше 18, функция должна бросить ошибку ValidationError с сообщением "Age must be at least 18" и полем "age".
 */
 
 export class ValidationError extends Error {
-	field: string;
-	constructor(message: string, field: string) {
-		super(message);
-		this.name = 'ValidationError';
-		this.field = field;
-	}
+field: string;
+constructor(message: string, field: string) {
+super(message);
+this.name = 'ValidationError';
+this.field = field;
+}
 }
 
 export function validateUserAge(age: number): void {
-	// Ваш код здесь
+if (age < 18) {
+throw new ValidationError('Age must be at least 18', 'age');
+}
 }
