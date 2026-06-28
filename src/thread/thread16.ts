bafdfaef80d9523dc3ac3c0b2ed4caadd36ced91
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:45 +0600

    thread16

--- src/thread/thread16.ts
+++ src/thread/thread16.ts
@@ -1,9 +1,14 @@
-/* 
-	Дописать функцию getAllUserInfo, которая возвращает результаты всех запросов независимо от их успешности.
+﻿/* 
+Дописать функцию getAllUserInfo, которая возвращает результаты всех запросов независимо от их успешности.
 */
 
 import { fetchUser, fetchUserData, fetchUserPosts, fetchUserSettings } from "./promises";
 
 export function getAllUserInfo(id: number): Promise<Array<PromiseSettledResult<any>>> {
-  // TODO: Использовать Promise.allSettled для получения всех результатов
+  return Promise.allSettled([
+    fetchUser(id),
+    fetchUserData(id),
+    fetchUserPosts(id),
+    fetchUserSettings(id)
+  ]);
 }
