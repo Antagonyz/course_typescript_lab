cfc050254d430c844e5aab15ef5e5909fb91eb95
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:44 +0600

    thread11

--- src/thread/thread11.ts
+++ src/thread/thread11.ts
@@ -1,10 +1,11 @@
-/* 
-	Дописать функцию getUserWithData, которая сначала получает пользователя, затем его данные и возвращает объект { user: User, data: User }.
+﻿/* 
+Дописать функцию getUserWithData, которая сначала получает пользователя, затем его данные и возвращает объект { user: User, data: User }.
 */
 
 import { fetchUser, fetchUserData, type User } from "./promises";
 
-export function getUserWithData(id: number): Promise<{ user: User; data: User }> {
-  // TODO: Реализовать последовательное получение пользователя и его данных
-
+export async function getUserWithData(id: number): Promise<{ user: User; data: User }> {
+  const user = await fetchUser(id);
+  const data = await fetchUserData(id);
+  return { user, data };
 }
