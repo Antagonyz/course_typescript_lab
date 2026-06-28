25f3b8acf6404fb1939bc1fc3f04bcc37941f3c5
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:19:12 +0600

    objects12

--- src/objects/objects12.ts
+++ src/objects/objects12.ts
@@ -4,11 +4,11 @@
 
 const calculator = {
     previousValue: 0,
-    add: (value: number) => {
+    add(value: number) {
         return this.previousValue += value;
     }
 };
 
 export function add(value: number) {
-	return calculator.add(value);
+    return calculator.add(value);
 }
