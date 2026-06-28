bd33a1fc843a74d2a75d19287e621e498ee95d54
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 00:50:37 +0600

    classes02

--- src/classes/classes02.ts
+++ src/classes/classes02.ts
@@ -6,7 +6,8 @@ export class Product {
   name: string;
   price: number;
 
-  constructor(name: string, price: number) {
-        
+  constructor(name: string, price: number) {    
+    this.name = name;
+    this.price = price < 0 ? 0 : price;
   }
 }
