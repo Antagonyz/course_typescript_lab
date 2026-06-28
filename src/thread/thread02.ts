a1f644444dbe20c16cfd1763885f22cf89eea86f
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:42 +0600

    thread02

--- src/thread/thread02.ts
+++ src/thread/thread02.ts
@@ -1,8 +1,12 @@
-/* 
-	Допишите функцию парсинга JSON, чтобы она возвращала null при некорректном JSON и логировала ошибку.
+﻿/* 
+Допишите функцию парсинга JSON, чтобы она возвращала null при некорректном JSON и логировала ошибку.
 */
 
 export function parseJsonSafely(jsonString: string): any | null {
-  // Ваш код здесь (3-5 строк)
-  return JSON.parse(jsonString);
+  try {
+    return JSON.parse(jsonString);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
