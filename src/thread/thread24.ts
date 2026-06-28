7afeac4820e54f50b3f87f0e423fd682f6661462
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:46 +0600

    thread24

--- src/thread/thread24.ts
+++ src/thread/thread24.ts
@@ -1,5 +1,5 @@
-/* 
-	Используйте orElse, чтобы попробовать альтернативный парсер если первый failed.
+﻿/* 
+Используйте orElse, чтобы попробовать альтернативный парсер если первый failed.
 */
 
 import { err, ok, type Result } from "neverthrow"
@@ -7,13 +7,9 @@ import { parseNumber } from "./fornever"
 
 // Альтернативный парсер
 const alternativeParser = (input: string): Result<number, string> => {
-	return input === "zero" ? ok(0) : err("Not zero")
+return input === "zero" ? ok(0) : err("Not zero")
 }
-
 
 export const tryParseNumber = (input: string): Result<number, string> => {
-	return parseNumber(input)
-	// Ваш код здесь 
+return parseNumber(input).orElse(() => alternativeParser(input))
 }
-
-
