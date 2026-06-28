d7a03cebaf3a49f167a095061514ce736571aff3
mikhail <romanov.mihail.var@bk.ru>
2026-06-28 23:41:57 +0600

    basic18

--- src/basic/basic18.js
+++ src/basic/basic18.js
@@ -3,6 +3,17 @@
 */
 
 export function second_largest(arr) {
-  return null;
+  let max = -Infinity;
+  let second = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      second = max;
+      max = arr[i];
+    } else if (arr[i] > second && arr[i] !== max) {
+      second = arr[i];
+    }
+  }
+  return second === -Infinity ? null : second;
 }
 
