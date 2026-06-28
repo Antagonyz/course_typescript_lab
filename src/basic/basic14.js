08bb92f3c38407d91a1457a91a124384e5bd3a96
mikhail <romanov.mihail.var@bk.ru>
2026-06-28 23:38:59 +0600

    basic14

--- src/basic/basic14.js
+++ src/basic/basic14.js
@@ -3,5 +3,9 @@
 */
 
 export function even_elements(arr) {
-    return [];
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) result.push(arr[i]);
+  }
+  return result;
 }
