93d8620d5697d7cb4e8c2a28ec5291c9b044d3b9
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:46 +0600

    thread23

--- src/thread/thread23.ts
+++ src/thread/thread23.ts
@@ -1,16 +1,18 @@
-/* 
-	Используйте mapErr, чтобы преобразовать ошибку из строки в более описательный объект. При этом установите код ошибки в 400, а сообщение в `Parse error: ${error}`
+﻿/* 
+Используйте mapErr, чтобы преобразовать ошибку из строки в более описательный объект. При этом установите код ошибки в 400, а сообщение в `Parse error: ${error}`
 */
 
 import type { Result } from "neverthrow";
 import { parseNumber } from "./fornever";
 
 interface AppError {
-	message: string;
-	code: number;
+message: string;
+code: number;
 }
 
 export const parseNumberWithDetailedError = (input: string): Result<number, AppError> => {
-	return parseNumber(input)
-	// Ваш код здесь (3-5 строк)
+return parseNumber(input).mapErr(error => ({
+message: `Parse error: ${error}`,
+code: 400
+}))
 }
