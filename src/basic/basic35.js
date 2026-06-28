7d07d6e585c7d50219eaf716574669db910199ac
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 00:34:14 +0600

    basic35

--- src/basic/basic35.js
+++ src/basic/basic35.js
@@ -3,6 +3,9 @@
 */
 
 export function diagonalSum(matrix) {
-	let sum = 0;
-	return sum;
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    sum += matrix[i][i];
+  }
+  return sum;
 }
