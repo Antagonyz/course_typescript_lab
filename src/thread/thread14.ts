d0b8484b9f4850757ea5166a1778fea07c6fcb92
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:45 +0600

    thread14

--- src/thread/thread14.ts
+++ src/thread/thread14.ts
@@ -1,11 +1,12 @@
-/* 
-	Дописать функцию getUserPostsWithAuthor, которая получает посты пользователя, а затем информацию об авторе каждого поста.
+﻿/* 
+Дописать функцию getUserPostsWithAuthor, которая получает посты пользователя, а затем информацию об авторе каждого поста.
 */
 
 import { fetchUser, fetchUserPosts, type Post } from "./promises";
 import type { User } from "./thread07";
 
-export function getUserPostsWithAuthor(userId: number): Promise<Array<Post & { author: User }>> {
-	// TODO: Реализовать цепочку промисов
-
+export async function getUserPostsWithAuthor(userId: number): Promise<Array<Post & { author: User }>> {
+  const posts = await fetchUserPosts(userId);
+  const author = await fetchUser(userId);
+  return posts.map(post => ({ ...post, author }));
 }
