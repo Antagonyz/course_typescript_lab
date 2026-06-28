0b61a8314e381cf27e75380f07fcbe83ec34c60b
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:45 +0600

    thread15

--- src/thread/thread15.ts
+++ src/thread/thread15.ts
@@ -1,10 +1,10 @@
-/* 
-	Дописать функцию getFastestUserData, которая возвращает результат первого завершившегося промиса (пользователь или данные пользователя).
-	Подсказка: используется Promise.race
+﻿/* 
+Дописать функцию getFastestUserData, которая возвращает результат первого завершившегося промиса (пользователь или данные пользователя).
+Подсказка: используется Promise.race
 */
 
 import { fetchUser, fetchUserData, type User } from "./promises";
 
 export function getFastestUserData(id: number): Promise<User> {
-  // TODO: Использовать Promise.race для получения самого быстрого результата
+  return Promise.race([fetchUser(id), fetchUserData(id)]);
 }
