40d986380fe6a2c2c441843a6465de4bf478bed1
mikhail <romanov.mihail.var@bk.ru>
2026-06-29 02:10:46 +0600

    thread20

--- src/thread/thread20.ts
+++ src/thread/thread20.ts
@@ -1,11 +1,10 @@
-/* 
-	Дан код, который преобразует строку в число. Доделайте функцию, чтобы она возвращала ошибку "Invalid number", если преобразование невозможно.
+﻿/* 
+Дан код, который преобразует строку в число. Доделайте функцию, чтобы она возвращала ошибку "Invalid number", если преобразование невозможно.
 */
 
 import { Result, err, ok } from 'neverthrow'
 
 export const parseNumber = (input: string): Result<number, string> => {
-	const num = Number(input)
-	// Ваш код здесь (3-5 строк)
-
+const num = Number(input)
+return Number.isNaN(num) ? err('Invalid number') : ok(num)
 }
